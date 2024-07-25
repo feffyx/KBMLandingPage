8568f8d04420f8f05650da2e5ad238e57951d945
Federica Ziaco <148449872+feffyx@users.noreply.github.com>
2024-07-25 15:43:52 +0100

    Add files via upload

---
+++ KBM-landing-page/README.md
@@ -0,0 +1,13 @@
+improve design
+fix carousel on mobile
+add video  https://www.youtube.com/watch?v=m_29tgwxTAg
+
+
+
+Dark blue #374785, navbar, headers, navigation bar, accents
+Light Gray #F2F2F2 backround for sections
+Warm yellow #FFC857 for buttons, highlights
+Coral red #E63946 for icons, important informations and alerts
+Dark Gray #333333 for text
+White #FFFFF for text on drk background
+
---
+++ KBM-landing-page/js/main.js
@@ -0,0 +1,23 @@
+// $(document).ready(function() {
+//     $('a[href*="#"]').on('click', function (e){
+//         e.preventDefault();
+//         $('html, body').animate({
+//             scrollTop: $($
+//                 (this).attr('href')).offset().top
+//         }, 500, 'linear');
+//     });
+// });
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     const hiddenElements = document.querySelectorAll('.hidden');
+  
+//     window.addEventListener('scroll', () => {
+//       hiddenElements.forEach((element) => {
+//         const top = element.offsetTop;
+//         const scrollPosition = window.scrollY + window.innerHeight;
+//         if (top < scrollPosition) {
+//           element.classList.remove('hidden');
+//         };
+//       });
+//     });
+//   });
